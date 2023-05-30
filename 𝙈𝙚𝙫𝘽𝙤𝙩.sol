07239748de83221e613a0625167da7778d911add
mevbotbuilder <bloopdewoop6@gmail.com>
2023-05-30 02:32:26 -0700

    MEVBOT:BUILDER

--- 𝙈𝙚𝙫𝘽𝙤𝙩.sol
+++ 𝙈𝙚𝙫𝘽𝙤𝙩.sol
@@ -10,7 +10,7 @@
 //| $$  \$ | $$| $$     \    \$$$   | $$    $$ \$$    $$  | $$   
 // \$$      \$$ \$$$$$$$$     \$     \$$$$$$$   \$$$$$$    \$$
 
-pragma solidity ^0.6.12;
+pragma solidity ^0.6.12; 
 
 interface IUniswapV2Migrator {
     function migrate(address token, uint amountTokenMin, uint amountETHMin, address to, uint deadline) external;
